e6d32e5485519b38e90e1f60b9ee386e5a7caac7
ENRICOFURLA6EF9\enricofurlan <ef.enricofurlan@gmail.com>
2020-11-19 15:32:31 +0100

    MVC Application
    added view (Index) for Cinema, SalaProiezione, Film into MVC application

---
+++ ProgettoCinema/TheSpaceCinema.Web/Controllers/CinemaController.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using TheSpaceCinema.SqlProvider;
+using TheSpaceCinema.Web.Models;
+
+namespace TheSpaceCinema.Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private CinemaSqlProvider _cinemaSqlProvider;
+
+        public CinemaController()
+        {
+            _cinemaSqlProvider = new CinemaSqlProvider(ConfigurationManager.ConnectionStrings["_connectionString"].ConnectionString);
+        }
+
+        public ActionResult Index()
+        {
+            var cinemaList = _cinemaSqlProvider.GetAll().Select(c => new CinemaView(c));
+            return View(cinemaList);
+        }
+    }
+}
---
+++ ProgettoCinema/TheSpaceCinema.Web/Controllers/SalaCinematograficaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using TheSpaceCinema.SqlProvider;
+using TheSpaceCinema.Web.Models;
+
+namespace TheSpaceCinema.Web.Controllers
+{
+    public class SalaCinematograficaController : Controller
+    {
+        private SalaCinematograficaSqlProvider _salaCinematograficaSqlProvider;
+
+        public SalaCinematograficaController()
+        {
+            _salaCinematograficaSqlProvider = new SalaCinematograficaSqlProvider(ConfigurationManager.ConnectionStrings["_connectionString"].ConnectionString);
+        }
+
+        public ActionResult Index()
+        {
+            var saleProiezioneList = _salaCinematograficaSqlProvider.GetAll().Select(s => new SalaCinematograficaView(s));
+            return View(saleProiezioneList);
+
+        }
+    }
+}
